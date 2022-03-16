aac9fc7df2732a748972abe5d6a981a0ace5d346
dudasc <dudatimbe@gmail.com>
2022-03-16 10:27:11 -0300

    FIX: theme preferences config

--- lib/app/modules/shared/theme/theme_preferences.dart
+++ lib/app/modules/shared/theme/theme_preferences.dart
@@ -1,19 +1,24 @@
-// ignore: import_of_legacy_library_into_null_safe
+// ignore_for_file: invalid_use_of_visible_for_testing_member, import_of_legacy_library_into_null_safe, non_constant_identifier_names
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemePreference {
-  // ignore: constant_identifier_names, non_constant_identifier_names
-  static String PREF_KEY = "prefKey";
+  static String DARK_THEME = "darkTheme";
+
+  ThemePreference() {
+    Map<String, Object> values = <String, Object>{'darkTheme': true};
+    SharedPreferences.setMockInitialValues(values);
+  }
 
   setTheme(bool value) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    
-    sharedPreferences.setBool(PREF_KEY, value);
+
+    sharedPreferences.setBool(DARK_THEME, value);
   }
 
   getTheme() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
 
-    return sharedPreferences.getBool(PREF_KEY) ?? false;
+    return sharedPreferences.getBool(DARK_THEME) ?? false;
   }
 }
